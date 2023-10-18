72ef3e993fa6fd4b6d1360101ece269e445b52f8
binhquocnguyen <binhquoc7504@gmail.com>
2023-10-18 19:51:03 +0700

    feat: Create 16x16 square grid

---
+++ script.js
@@ -0,0 +1,29 @@
+const gridContainer = document.querySelector('.grid-container'); 
+
+function createGridSquare() {
+    for (let i = 1; i <= 16; i++) { //each column
+        const gridColumn = document.createElement('div');
+        gridColumn.className = 'grid-column';
+        gridContainer.appendChild(gridColumn);
+
+        for (let j = 1; j <= 16; j++) { //each row
+            const squareBox = document.createElement('div');
+            gridColumn.appendChild(squareBox);
+            
+            /* Boxes' borders */
+            squareBox.style.borderRight = '1px solid blue'; //test (color)
+
+            squareBox.style.borderBottom = '1px solid green'; //test (color)
+            
+            if (i == 1) {
+                squareBox.style.borderLeft = '1px solid red'; //test (color)
+            } 
+            
+            if (j == 1) {
+                squareBox.style.borderTop = '1px solid turquoise'; //test (color)
+            }
+        } 
+    }
+}
+
+createGridSquare();
